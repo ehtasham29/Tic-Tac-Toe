5e39f17cb4c76be33c7155a374540abecd929136
Ehtasham <ehtasham8292@gmail.com>
2023-07-21 18:43:18 +0530

    first change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
-let turn = "X" 
+let turn = "X" ;
 
 // function to change the turn 
 const changeTurn = ()=> {
@@ -18,15 +18,15 @@ const checkWin = ()=> {
 // Game logic 
 let boxes = document.getElementsByClassName("box") ;
 Array.from(boxes).forEach(element => {
-    let boxtext = document.querySelector('.boxtext') ;
+    let boxtext = element.querySelector('.boxtext') ;
     element.addEventListener('click', () => {
         if(boxtext.innerText === '')
         {
-            boxtext.innerText = "X" ;
+            boxtext.innerText = turn ;
             changeTurn() ;
             audioTurn.play() ;
             checkWin() ;
-            document.getElementsByClassName("turn")[0].innerText = "Turn for " + turn ;
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn ;
 
         }
 
